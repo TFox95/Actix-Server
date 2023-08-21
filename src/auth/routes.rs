283ee93a1913b7724851a7fb65d0e9ff85e5f211
Thomas Fox <62257263+TFox95@users.noreply.github.com>
2023-08-20 21:41:57 -0500

    Changed the way to access the username attribute from the token on line 152. Changes to tag along with the AccessTokenSchema.

--- src/auth/routes.rs
+++ src/auth/routes.rs
@@ -149,7 +149,7 @@ async fn token_refresh(req: HttpRequest) -> Result<impl Responder, Box<dyn Error
 async fn retrieve_user(request: HttpRequest) -> Result<impl Responder, Box<dyn Error>> {
 
     let handle_token = check_auth_header(request, "Authorization").await?;
-    let handle_user = UserCRUD.retrieve_user_by_username_or_email(&get_db_pool(SQLITE_TUPLE).await?, handle_token.username).await?;
+    let handle_user = UserCRUD.retrieve_user_by_username_or_email(&get_db_pool(SQLITE_TUPLE).await?, handle_token.data.username).await?;
 
     let jsonable = json::object! {
         "data" => json::object! {
